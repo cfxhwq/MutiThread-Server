3de2026e8de6bf430eba3a8d14264a230e6cc5da
cfxhwq <xmhwq@sina.com>
2015-03-08 21:39:07 +0800

    finish TSD

--- src/MutiThread-Server.cpp
+++ src/MutiThread-Server.cpp
@@ -23,13 +23,28 @@ using namespace std;
 
 void process_cli(int connectfd, struct sockaddr_in client);
 void *function1(void *arg);
-void savedata(char* recvbuf, int len, char * cli_data,int &index);
+void savedata(char* recvbuf, int len, char * cli_data);
 
 struct ARG {
 	int connfd;
 	struct sockaddr_in client;
 };
-//instance
+
+pthread_key_t key;
+pthread_once_t once = PTHREAD_ONCE_INIT;
+static void destructor(void* ptr) {
+	cout << "ptr" << ptr << endl;
+	free(ptr);
+	cout << "exec destructor" << endl;
+}
+struct ST_DATA {
+	int index;
+};
+static void creatkey_once(void) {
+	cout << "exec creatkey_once" << endl;
+	pthread_key_create(&key, destructor);
+}
+
 int main() {
 	int listenfd, connectfd;
 	pthread_t tid;
@@ -96,11 +111,12 @@ void* function1(void* arg) {
 	info = (struct ARG*) arg;
 	process_cli(info->connfd, info->client);
 	free(arg);
+	cout << "arg" << arg << endl;
 	pthread_exit(NULL);
 }
 
 void process_cli(int connectfd, struct sockaddr_in client) {
-	int num,index=0;
+	int num, index = 0;
 	char cli_data[1000];
 	char recvbuf[MAXDATASIZE], sendbuf[MAXDATASIZE], cli_name[MAXDATASIZE];
 	printf("process from %s\n", inet_ntoa(client.sin_addr));
@@ -117,7 +133,7 @@ void process_cli(int connectfd, struct sockaddr_in client) {
 		printf("rec msg len:%d\n", num);
 		recvbuf[num - 1] = '\0';
 		printf("Client %s Msg: %s\n", cli_name, recvbuf);
-		savedata(recvbuf, num, cli_data, index);
+		savedata(recvbuf, num, cli_data);
 		int i = 0;
 		for (i = 0; i < num - 1; ++i) {
 			if ((recvbuf[i] >= 'a' && recvbuf[i])
@@ -133,16 +149,25 @@ void process_cli(int connectfd, struct sockaddr_in client) {
 		send(connectfd, sendbuf, strlen(sendbuf), 0);
 	}
 	close(connectfd);
-	printf("Client %s , data:%s!\n",cli_name,cli_data);
+	printf("Client %s , data:%s!\n", cli_name, cli_data);
 }
 
-void savedata(char* recvbuf,int len,char* cli_data,int &index){
-	//static int index =0;
-	int i=0;
-	while(i<len-1){
-		cli_data[index++] = recvbuf[i];
+void savedata(char* recvbuf, int len, char* cli_data) {
+	struct ST_DATA *data;
+	pthread_once(&once, creatkey_once);
+	cout << "key" << &key << endl;
+	if ((data = (struct ST_DATA*) pthread_getspecific(key)) == NULL) {
+		cout << "reate data" <<endl;
+		data = (struct ST_DATA *) malloc(sizeof(struct ST_DATA));
+		pthread_setspecific(key,data);
+	}
+	cout << "getDATA" << data << (data == NULL) << endl;
+
+	int i = 0;
+	while (i < len - 1) {
+		cli_data[data->index++] = recvbuf[i];
 		i++;
 	}
-	cli_data[index]='\0';
+	cli_data[data->index] = '\0';
 }
 
